7a9a6aebdf6c01b728f0c5ef9d8383c1f6e39a5f
Likitha258 <71436387+Likitha258@users.noreply.github.com>
2022-11-30 10:02:08 +0000

    hol2 uploaded successfully..

---
+++ HOL2/HomeController1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HOL2.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["str1"] = "This is a string passed using ViewData";
+
+            ViewData["num1"] = 100;
+
+            ViewBag.str2 = "This is a string passed using ViewBag";
+
+            ViewBag.num2 = 200;
+
+            return View();
+        }
+    }
+}
